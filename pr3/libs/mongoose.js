b12580228ea36b0a448cfce396e3f241229bf270
MykolaMeln <Kolamelnychuk2000@gmail.com>
2019-09-24 09:32:59 +0300

    probily i taby

--- pr3/libs/mongoose.js
+++ pr3/libs/mongoose.js
@@ -3,15 +3,14 @@ var log = require('./log')(module);
 
 var config = require('./config');
 mongoose.connect(config.get('mongoose:uri'),{ useUnifiedTopology: true,useNewUrlParser:true});
-//mongoose.connect('mongodb://localhost/test1',{  useUnifiedTopology: true,useNewUrlParser:true});
 var db = mongoose.connection;
 
 db.on('error', function (err) {
-log.error('connection error:', err.message);
+    log.error('connection error:', err.message);
 });
 
 db.once('open', function callback () {
-console.log("Connected to DB!");
+    console.log("Connected to DB!");
 });
 
 var Schema = mongoose.Schema;
@@ -22,25 +21,17 @@ type: String,
 enum: ['thumbnail', 'detail'],
 required: true },
 url: { type: String, required: true }
-});
-/*
+});*/
 var Article = new Schema({
 title: { type: String, required: true },
 author: { type: String, required: true },
 description: { type: String, required: true },
-images: [Images],
 modified: { type: Date, default: Date.now }
-});*/
-var Article = new Schema({
-//title: { type: String, required: true },
-author: { type: String, required: true }
-//description: { type: String, required: true },
-//modified: { type: Date, default: Date.now }
 });
-/*
+
 Article.path('title').validate(function (v) {
   return v.length > 5 && v.length < 70;
 });
-*/
+
 var ArticleModel = mongoose.model('Article', Article);
 module.exports.ArticleModel = ArticleModel;
